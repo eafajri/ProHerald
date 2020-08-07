eef082bb7aae2ec700a9344427570f515e0b69ab
erric alfajri <erric.alfajri@bukalapak.com>
2020-08-07 19:02:40 +0700

    Creating single view app with xcode

---
+++ ProHerald/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProHerald
+//
+//  Created by erric alfajri on 07/08/20.
+//  Copyright © 2020 proherald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
