6e37a475a51c12304ad88dc76b4dbfe719e0ffcf
erric alfajri <erric.alfajri@bukalapak.com>
2020-08-07 22:56:14 +0700

    revert commits on /Pods

--- Pods/Target Support Files/Pods-ProHerald-ProHeraldUITests/Pods-ProHerald-ProHeraldUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProHerald_ProHeraldUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ProHerald_ProHeraldUITests
-@end
--- Pods/Target Support Files/Pods-ProHeraldTests/Pods-ProHeraldTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProHeraldTests : NSObject
-@end
-@implementation PodsDummy_Pods_ProHeraldTests
-@end
